0c82f7c5a0954e1d4bfad374007b28b2235e4468
ODAJIM17 <Davila56jd@gmail.com>
2024-02-23 10:57:09 -0500

    Created Daatabase Oders

---
+++ Orders/Orders.Shared/Entities/Country.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Orders.Shared.Entities
+{
+    public class Country
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Country")]
+        [MaxLength(100, ErrorMessage = "Field {0} allows max {1} characters")]
+        [Required(ErrorMessage = "Field {0} is required.")]
+        public string? Name { get; set; }
+
+        // public ICollection<State> States { get; set; }
+
+        // [Display(Name = "State Count")]
+        // public int StatesCount => States == null ? 0 : States.Count;
+
+        ///[Display(Name = "Cities")]
+        // public int CitiesCount => States == null ? 0 : States.Sum(s => s.CitiesCount);
+    }
+}
